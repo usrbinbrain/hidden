4c7de1e58e1940fb891a09cf6b3d467e257329d6
Fedora0fC <tr4kthebox@gmail.com>
2020-10-19 14:15:22 -0300

    h1dd3n.sh Update.

--- h1dd3n.sh
+++ h1dd3n.sh
@@ -1,29 +1,39 @@
 #!/usr/bin/env bash
-###############################################
-## Hidden service trought onion network.
-###############################################
+########################################
+# Hidden service trought onion network.
+########################################
 
+# Function to make configuration file of tor package.
 hdir() {
         echo -e "# Hidden service on local port "$1".\nHiddenServiceDir /var/lib/tor/"$1"_service/\nHiddenServicePort "$1" 127.0.0.1:"$1"\n\n" >> /etc/tor/torrc
         mkdir /var/lib/tor/"$1"_service > /dev/null
         chown debian-tor:debian-tor /var/lib/tor/"$1"_service/
 }
-
+# Check tor package.
 whereis tor > /dev/null && torok="."
-
+# Get $torok status.  
 if [ "$torok" == "." ]; then
-        if [ -z "$1" ]; then
-                echo -e "USE: $0 <service_port>\n" && exit
+        # Get first parameter status.
+	if [ -z "$1" ]; then
+                # Show information usage.
+		echo -e "USE: $0 <service_port>\n" && exit
         fi
         mtf=$1
         hdir $mtf
-        systemctl stop tor &&
-        chmod 700 /var/lib/tor/"$mtf"_service/ &&
-        sudo -u debian-tor tor & disown
+        # Stop tor if runnning.
+	systemctl stop tor &&
+        # Assigning permissions.
+	chmod 700 /var/lib/tor/"$mtf"_service/ &&
+        # Launch tor as debian-tor user on background mode.
+	sudo -u debian-tor tor & disown
         sleep 5
-        onionway=$(cat /var/lib/tor/"$mtf"_service/hostname)
-        echo -e "\n\n[-] Service 127.0.0.1:$mtf avaliable on: $onionway:$mtf\n"
-        unset torok
+        # Get URL of service on tor network.
+	onionway=$(cat /var/lib/tor/"$mtf"_service/hostname)
+        # Show access information.
+	echo -e "\n\n[-] Service 127.0.0.1:$mtf avaliable on: $onionway:$mtf\n"
+        # Unset $torok.
+	unset torok
 else
-        echo "[-] Need install tor package !"
+        # Show this message if tor package does not exist.
+	echo "[-] Need install tor package !"
 fi
